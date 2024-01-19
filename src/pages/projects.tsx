a8a4c079d8a49528e752f6b83baa9725444e256f
rushik1903 <sairushik.jasti@gmail.com>
2024-01-19 12:34:43 +0530

    route checking

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,7 +17,7 @@ function Projects(){
                             <div style={{padding:"20px"}}>
                                 <h3>Wrist Try-On</h3>
                                 <p>Making a wrist try on by leveraging mediapipe</p>
-                                <button>try demo</button>
+                                <button><a href="../../wristTryOn.html">try demo</a></button>
                             </div>
                         </div>
                     </div>
